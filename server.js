3b5d33b37ecdfed3ab4782915e1f361f3e0f6ee6
Ifeanyi (Project Manager) <md@specialmansolution.com>
2021-02-15 12:27:58 +0100

    updated ssl

--- server.js
+++ server.js
@@ -6,9 +6,30 @@ const app = require('./app');
 const port = process.env.PORT;
 
 const server = http.createServer(app);
+
+const https = require('https');
+
   
 
 // Connects to mongodb
 createDbConnection();
 
-server.listen(port);
+var https_options = {
+
+    key: fs.readFileSync("./api.uatdrive.com.key.txt"),
+
+    cert: fs.readFileSync("./api.uatdrive.com.crt"),
+
+    ca: [
+
+        fs.readFileSync('./addtrustexternalcaroot.crt'),
+
+        fs.readFileSync('./api.uatdrive.com.ca-bundle')
+
+    ]
+};
+
+//    http.createServer(https_options, app)
+//    .listen(port);
+  https.createServer(https_options, app)
+   .listen(port);
